8ef37bc59bfa7ffab2b69740267e75cd3299c244
BenasdTW <ben6312tt@gmail.com>
2019-04-21 02:18:31 +0800

    'this' problem need to be solved

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 // Retrieve remote BrowserWindow
 const { BrowserWindow } = require("electron").remote;
+function add_new(s) {
+    let node = $("<p></p>").text(s).attr({ "id": "to_do", "class": "last" });
+    let prev = $("p.last");
+    //console.log(prev);
+    prev.before(node);
+    prev.removeClass("last");
+    $("p#first").remove();
+    //let all_node = $("#content p");
+    //console.log(all_node);
+    $("#content p").each((index) => {
+        console.log(index.toString() + ": " + $(this).text());
+        $(this).text(index.toString() + ": " + $(this).text());
+    });
+}
 $(document).ready(function () {
     $("#min-btn").on("click", () => {
         BrowserWindow.getFocusedWindow().minimize();
@@ -7,10 +21,11 @@ $(document).ready(function () {
     $("#close-btn").on("click", () => {
         BrowserWindow.getFocusedWindow().close();
     });
-    $("#input-box").keypress(function (e) {
+    $("#input-box").keypress((e) => {
         let input_value = $("#input-box").val().toString();
         if (e.keyCode == 13) {
-            $("#content p").html(input_value);
+            //$("#content p").html(input_value);
+            add_new(input_value);
         }
     });
 });
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,21 @@
 
 const {BrowserWindow} = require("electron").remote
 
+function add_new(s: string): void{
+    let node = $("<p></p>").text(s).attr({"id": "to_do", "class": "last"});
+    let prev = $("p.last");
+    //console.log(prev);
+    prev.before(node);
+    prev.removeClass("last");
+    $("p#first").remove();
+
+    //let all_node = $("#content p");
+    //console.log(all_node);
+    $("#content p").each((index) => {
+        console.log(index.toString()+": "+$(this).text());
+        $(this).text(index.toString()+": "+$(this).text()); 
+    });
+}
 
 $(document).ready(function() {
     $("#min-btn").on("click", () => {
@@ -11,10 +26,11 @@ $(document).ready(function() {
         BrowserWindow.getFocusedWindow().close();
     });
 
-    $("#input-box").keypress(function(e){
+    $("#input-box").keypress((e) => {
         let input_value: string = $("#input-box").val().toString();
         if(e.keyCode==13){
-            $("#content p").html(input_value);
+            //$("#content p").html(input_value);
+            add_new(input_value);
         }
     });
 });
